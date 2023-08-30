64e01b645d2ba439d796e6a905c47a4255263392
hyejin beck <beckhye@gmail.com>
2023-08-30 14:02:14 +0900

    login로직

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm 
+# AuthenticationForm 추가 
 from .models import User 
 from django.contrib.auth import get_user_model 
 
@@ -9,3 +10,6 @@ class CustomUserCreationForm(UserCreationForm):
         # 다만, get_user_model() 쓰는게 유지보수하기 좋다. 
         fields = ('username', 'profile_image',) 
         # profile_image = 사용자가 회원가입할때 프로필사진 올릴수있도록 
+
+class CustomAuthenticationForm(AuthenticationForm): 
+    pass 
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect # redirect 추가 
-from .forms import CustomUserCreationForm 
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm , CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
@@ -20,3 +21,18 @@ def signup(request):
     # 아직 form.html 파일을 만들지 않았는데 , 
     # 여기까지 후 작동하게 되면 작성할수있는 어떤 form이 나온다! 
     # 이때 나오는건, posts>templates>forms.html 
+
+def login(request): 
+    if request.method == 'POST': 
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid(): 
+            user = form.get_user()
+            auth_login(request,user)
+            return redirect('posts:index')
+    else: 
+        form = CustomAuthenticationForm() 
+    
+    context = {
+        'form': form, 
+    }
+    return render(request, 'accounts/form.html', context)
