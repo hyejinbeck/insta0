853bbae054f70ebfedd29a96a19f0502ca3cd168
hyejin beck <beckhye@gmail.com>
2023-08-30 11:10:35 +0900

    3. create구현완료(이미지포스팅)

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # redirect추가 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +12,19 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def create(request): 
+    if request.method == 'POST': 
+        form = PostForm(request.POST, request.FILES)
+        # request.POST 사용자가 입력한 사진 
+        # request.FILES 그 사진을 파일화 함 
+        if form.is_valid(): 
+            form.save() 
+            return redirect('posts:index')
+    else: 
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request,'form.html',context)
